b2e7c3ae516d2654567942baf894a8884ac2258e
aniketsingh1023 <aniketsinghn10@gmail.com>
2026-03-12 18:13:02 +0530

    fix: Learning More Postgres querires and read docs

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,10 @@ const pgClient = new Client(process.env.DATABASE_URI);
 
 async function main() {
     await pgClient.connect();
-    const res = await pgClient.query("SELECT * FROM users");
+    // const res = await pgClient.query("SELECT * FROM users");
+    const res = await pgClient.query("INSERT INTO users(username,email,password) VALUES('priya','priya@pandamail.com','Priya')");
     console.log(res.rows);
- 
 };
+
 
 main();
